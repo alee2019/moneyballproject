c8880efef2640953eb8fefcd3790700efd9f6638
Akshaj Bansal <AJ@huntsman-ve702-6340.apng.wlan.private.upenn.edu>
2018-07-23 13:02:29 -0400

    selecteddata

--- data/analysisRScript.R
+++ data/analysisRScript.R
@@ -4,7 +4,7 @@ library(tidyverse)
 combined <- data_complete %>%
   select(PLAYERID, TEAM, GP.x, GS.x, MPG.x,MPG.y, DEF_RTG.x,DEF_RTG.y, DEF_REB_PCT.x,DEF_REB_PCT.y, STL_PCT.x,STL_PCT.y,BLK_PCT.x,BLK_PCT.y, DEFLECTIONS_PER_48,Year)
 combined <- combined %>%
-  filter(MPG.y >= 15, MPG.x >= 15, GP.y >=15, GP.x >= 40)
+  filter(MPG.y >= 15, MPG.x >= 15, GP.y >=15, GP.x >= 40, DEF_RTG.y >= 0, DEF_RTG.x >= 0)
 fit <- lm(DEF_RTG.x ~ DEF_RTG.y, data = combined)
 fit
 g <- ggplot(data = combined) +
@@ -13,3 +13,4 @@ g <- ggplot(data = combined) +
 geom_smooth(aes(x = DEF_RTG.y, DEF_RTG.x), method = "lm", se = F)
 g
 cor(combined[['DEF_RTG.y']], combined[['DEF_RTG.x']])
+save(combined, file = "combined.RData")
