ad6de47ccbf713d6471ed6283de952c5c4f208cd
Akshaj Bansal <AJ@nichols-ve702-5374.apng.wlan.private.upenn.edu>
2018-07-23 19:45:29 -0400

    dumb dumb bubble gum

--- data/Untitled.R
+++ data/Untitled.R
@@ -1,1 +1,8 @@
 library(tidyverse)
+combine_analysis <- qualified_nba
+fit <- lm(DEF_RTG.x ~ z_HEIGHT, data = combine_analysis)
+fit
+height <- ggplot(data = combine_analysis) + 
+  geom_point(mapping = aes(x = z_HEIGHT, zBLK_PCT_NBA_year), size = 0.3) + 
+  geom_smooth(aes(x = z_HEIGHT, zBLK_PCT_NBA_year), method = 'lm', se = F)
+height
