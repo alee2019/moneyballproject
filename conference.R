416416659d613fbc4246510412cf20f0341060f8
Andrew Lee <andrewlee@nichols-ve702-4497.apng.wlan.private.upenn.edu>
2018-07-23 21:53:06 -0400

    conferences for errybody

--- conference.R
+++ conference.R
@@ -1,46 +1,23 @@
 library(tidyverse)
-Big12 <- c("KAN", "OKST", "OU", "BAYLOR", "TEXAS")
-ACC <- c("UVA", "DUKE", "CLEM", "UNC", "MIA", "FSU", "LOU", "SYR", "BC", "GT", "WAKE", "PITT")
-BigEast <- c("XAVIER", "NOVA", "SHU", "CREIGH", "PROV", "BUTLER", "MARQ", "GTOWN")
-BigTen <- c("MSU", "PUR", "OSU", "MICH", "NEB", "Penn State", "IND", "MD", "WIS", "Northwestern", "Minnesota", "ILL", "IOWA", "Rutgers")
-SEC <- c("AUB", "Tennessee", "FLA", "UK", "ARK", "MIZZOU", "Mississippi State", "Texas A&M", "ALA", "LSU", "Georgia", "USC", "VANDY", "Ole Miss")
-TheAmerican <- c("CIN", "HOU", "WICH", "Tulsa", "MEM", "UCF", "TEMPLE", "UCONN", "SMU", "Tulane", "East Carolina", "South Florida")
-PAC12 <- c("ARIZ", "USC", "UTAH", "UCLA", "STAN", "ORE", "Washington", "ASU", "Colorado", "Oregon State", "CAL") 
+Big12 <- c("KAN", "OKST", "OU", "BAYLOR", "TEXAS", "TTU", "WVU", "KSU", "TCU", "ISU")
+ACC <- c("UVA", "DUKE", "CLEM", "UNC", "MIA", "FSU", "LOU", "SYR", "BC", "GT", "WAKE", "PITT", "NCST", "VT", "ND", "BC")
+BigEast <- c("XAVIER", "NOVA", "SHU", "CREIGH", "PROV", "BUTLER", "MARQ", "GTOWN", "SJC", "DPU")
+BigTen <- c("MSU", "PUR", "MICH", "NEB", "PSU", "IND", "MD", "WIS", "NU", "MINN", "ILL", "IOWA", "RUTG")
+SEC <- c("AUB", "TENN", "FLA", "UK", "ARK", "MIZZOU", "MSU", "TXA&M", "ALA", "LSU", "UGA", "USC", "VANDY", "OMISS")
+TheAmerican <- c("CIN", "HOU", "WICH", "TULSA", "MEM", "UCF", "TEMPLE", "UCONN", "SMU", "TULANE", "ECU", "USF")
+PAC12 <- c("ARIZ", "USC", "UTAH", "UCLA", "STAN", "ORE", "WASH", "ASU", "CU", "OSU", "WSU", "CAL") 
 
-load("data/qualified_standardized_guards.RData")
-load("data/qualified_standardized_forwards.RData")
-load("data/qualified_standardized_centers.RData")
+load("data/ncaa_defense.RData")
 
-guards <- guards %>% mutate(CONF_NCAA = case_when(
-  TEAM_NCAA %in% Big12 ~ "Big 12", 
-  TEAM_NCAA %in% ACC ~ "ACC",
-  TEAM_NCAA %in% BigEast ~"Big East",
-  TEAM_NCAA %in% BigTen ~ "Big Ten",
-  TEAM_NCAA %in% SEC ~ "SEC",
-  TEAM_NCAA %in% TheAmerican ~ "The American",
-  TEAM_NCAA %in% PAC12 ~ "PAC 12"
+ncaa_data <- ncaa_data %>% mutate(CONF_NCAA = case_when(
+  Team %in% Big12 ~ "Big 12", 
+  Team %in% ACC ~ "ACC",
+  Team %in% BigEast ~"Big East",
+  Team %in% BigTen ~ "Big Ten",
+  Team %in% SEC ~ "SEC",
+  Team %in% TheAmerican ~ "The American",
+  Team %in% PAC12 ~ "PAC 12"
 ))
 
-forwards <- forwards %>% mutate(CONF_NCAA = case_when(
-  TEAM_NCAA %in% Big12 ~ "Big 12", 
-  TEAM_NCAA %in% ACC ~ "ACC",
-  TEAM_NCAA %in% BigEast ~"Big East",
-  TEAM_NCAA %in% BigTen ~ "Big Ten",
-  TEAM_NCAA %in% SEC ~ "SEC",
-  TEAM_NCAA %in% TheAmerican ~ "The American",
-  TEAM_NCAA %in% PAC12 ~ "PAC 12"
-))
+save(ncaa_data, file = "data/ncaa_defense.RData")
 
-centers <- centers %>% mutate(CONF_NCAA = case_when(
-  TEAM_NCAA %in% Big12 ~ "Big 12", 
-  TEAM_NCAA %in% ACC ~ "ACC",
-  TEAM_NCAA %in% BigEast ~"Big East",
-  TEAM_NCAA %in% BigTen ~ "Big Ten",
-  TEAM_NCAA %in% SEC ~ "SEC",
-  TEAM_NCAA %in% TheAmerican ~ "The American",
-  TEAM_NCAA %in% PAC12 ~ "PAC 12"
-))
-
-save(guards, file = "data/qualified_standardized_guards.RData")
-save(forwards, file = "data/qualified_standardized_forwards.RData")
-save(centers, file = "data/qualified_standardized_centers.RData")
