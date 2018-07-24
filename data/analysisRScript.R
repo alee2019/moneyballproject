f612fac2e14441aefd8f7118249ffb48617674e4
Akshaj Bansal <AJ@nichols-ve702-5374.apng.wlan.private.upenn.edu>
2018-07-23 21:58:09 -0400

    salty andrew

--- data/analysisRScript.R
+++ data/analysisRScript.R
@@ -1,13 +1,24 @@
 library(tidyverse)
 combined <- data_complete %>%
-  select(PLAYERID, POS, Year.x, Year, Team, TEAM, GP.x, GS.x, GP.y, GS.y, MPG.x,MPG.y, DEF_RTG.x,DEF_RTG.y, DEF_REB_PCT.x,DEF_REB_PCT.y, STL_PCT.x,STL_PCT.y,BLK_PCT.x,BLK_PCT.y, Agility, Shuttle, Sprint, Standing_Vertical, Max_Vertical, STANDING_REACH, Bench, HAND_LENGTH, HAND_WIDTH, HEIGHT_NO_SHOES, HEIGHT_SHOES, WEIGHT, WINGSPAN, ALL_DEFENSE)
-combined <- combined %>%
-  filter(MPG.y >= 15, MPG.x >= 15, GP.y >=15, GP.x >= 40, DEF_RTG.y >= 0, DEF_RTG.x >= 0)
-fit <- lm(DEF_RTG.x ~ DEF_RTG.y, data = qualified_nba)
-fit
-g <- ggplot(data = qualified_nba) +
-  geom_point(aes(x = DEF_RTG.y, y = DEF_RTG.x), size = 0.3, alpha = .3)+
-  geom_smooth(aes(x = DEF_RTG.y, DEF_RTG.x), method = "lm", se = F)
-g
-cor(qualified_nba[['DEF_RTG.y']], qualified_nba[['DEF_RTG.x']])
-save(combined, file = "combined.RData")
+  select(PLAYERID, POS, YEAR_NBA, YEAR_NCAA, TEAM_NBA, TEAM_NCAA, GP_NBA, GS_NBA, GP_NCAA, GS_NCAA, MPG_NBA,MPG_NCAA, DEF_RTG_NBA,DEF_RTG_NCAA, STL_PCT_NBA,STL_PCT_NCAA,BLK_PCT_NBA,BLK_PCT_NCAA , Agility, Shuttle, Sprint, Standing_Vertical, Max_Vertical, STANDING_REACH, Bench, HAND_LENGTH, HAND_WIDTH, HEIGHT_NO_SHOES, HEIGHT_SHOES, WEIGHT, WINGSPAN)
+library(tidyverse)
+
+combined_centers <- centers %>%
+  filter(MPG_NBA >= 15,  GP_NBA >= 40,  DEF_RTG_NBA >= 0)
+combined_forwards <- forwards %>%
+  filter(MPG_NBA >= 15,  GP_NBA >= 40,  DEF_RTG_NBA >= 0)
+combined_guards <- guards %>%
+  filter(MPG_NBA >= 15,  GP_NBA >= 40,  DEF_RTG_NBA >= 0)
+graph_centers <- ggplot(data = combined_centers) +
+  geom_point(aes(x = DEF_RTG_NCAA, y = DEF_RTG_NBA), size = 0.3, alpha = .3)+
+  geom_smooth(aes(x = DEF_RTG_NCAA, DEF_RTG_NBA), method = "lm", se = F)
+graph_forwards <- ggplot(data = combined_forwards) +
+  geom_point(aes(x = DEF_RTG_NCAA, y = DEF_RTG_NBA), size = 0.3, alpha = .3)+
+  geom_smooth(aes(x = DEF_RTG_NCAA, DEF_RTG_NBA), method = "lm", se = F)
+graph_guards <- ggplot(data = combined_guards) +
+  geom_point(aes(x = DEF_RTG_NCAA, y = DEF_RTG_NBA), size = 0.3, alpha = .3)+
+  geom_smooth(aes(x = DEF_RTG_NCAA, DEF_RTG_NBA), method = "lm", se = F)
+graph_centers
+graph_forwards
+graph_guards
+cor(centers[['DEF_RTG_NCAA']], centers[['DEF_RTG_NBA']])
