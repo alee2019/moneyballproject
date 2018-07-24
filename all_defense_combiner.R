2c4a14bf276263b68f18966705b8d4c00ad84ec5
leojacoby <leo_jacoby@branson.org>
2018-07-23 21:57:01 -0400

    all defense in data complete w standardized ncaa stats

--- all_defense_combiner.R
+++ all_defense_combiner.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-load("data/nba_college_combine_all.RData")
+load("data/data_complete.RData")
 load("data/all_defense_players.RData")
 
 all_teams <- rename(all_teams, Year.x = Season, ALL_DEFENSE = Team)
