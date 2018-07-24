6c3f66f5e205fefa4c01ab152da35e50fa4be4e2
leojacoby <leo_jacoby@branson.org>
2018-07-23 20:11:37 -0400

    3 seperate files for positions with sauce castillo

--- data_standardizer.R
+++ data_standardizer.R
@@ -7,14 +7,24 @@ standardize <- function(x) {
 }
 
 qualified_nba <- combined %>% 
-  filter(!is.na(HEIGHT_NO_SHOES), !is.na(STL_PCT.x), !is.na(BLK_PCT.x), !is.nan(DEF_RTG.x), GP.x >= 40, MPG.x >= 15.0)
+  filter(!is.na(POS), !is.na(STL_PCT.x), !is.na(BLK_PCT.x), !is.nan(DEF_RTG.x), GP.x >= 40, MPG.x >= 15.0)
 
 qualified_nba[qualified_nba == '-'] <- NA
 
 qualified_nba <- qualified_nba %>% mutate(HEIGHT_GROUP = case_when(
-  HEIGHT_NO_SHOES <= 76.0 ~ 1,
-  HEIGHT_NO_SHOES > 76.0 & HEIGHT_NO_SHOES <= 81.0 ~ 2,
-  HEIGHT_NO_SHOES > 81.0 ~ 3
+  POS == "PG" ~ 1,
+  POS == "PG-SG" ~ 1,
+  POS == "SG-PG" ~ 1,
+  POS == "SG" ~ 1,
+  POS == "SG-SF" ~ 2,
+  POS == "SF-SG" ~ 2,
+  POS == "SF" ~ 2,
+  POS == "SF-PF" ~ 2,
+  POS == "PF-SF" ~ 2,
+  POS == "PF" ~ 2,
+  POS == "PF-C" ~ 3,
+  POS == "C-PF" ~ 3,
+  POS == "C" ~ 3
 ))
 
 qualified_nba <- qualified_nba %>% 
@@ -30,6 +40,14 @@ qualified_nba <- qualified_nba %>%
 #qualified_nba <- qualified_nba %>% 
 #  filter(!is.nan(zDEF_RTG_NBA_year), !is.na(zDEF_RTG_NBA_year))
 
-save(qualified_nba, file = "data/qualified_standardized_combined.RData")
+guards <- qualified_nba %>% filter(HEIGHT_GROUP == 1)
+forwards <- qualified_nba %>% filter(HEIGHT_GROUP == 2)
+centers <- qualified_nba %>% filter(HEIGHT_GROUP == 3)
+
+
+
+save(guards, file = "data/qualified_standardized_guards.RData")
+save(forwards, file = "data/qualified_standardized_forwards.RData")
+save(centers, file = "data/qualified_standardized_centers.RData")
 
 
